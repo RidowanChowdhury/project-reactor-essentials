3a4908f5e5f5c0304972744e22941718136f693d
ridwan <ridwan.ramanujon@gmail.com>
2022-08-08 02:08:49 +0600

    first test

---
+++ src/test/java/FluxTest.java
@@ -0,0 +1,12 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FluxTest {
+
+
+    @Test
+    public void test() {
+        log.info("test!!");
+    }
+}
