c817d370e3fef0ee030ec8b39842f81ae0a3e887
ridwan <ridwan.ramanujon@gmail.com>
2022-08-08 02:38:56 +0600

    added new tests

--- src/test/java/FluxTest.java
+++ src/test/java/FluxTest.java
@@ -1,5 +1,7 @@
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class FluxTest {
@@ -8,5 +10,24 @@ public class FluxTest {
     @Test
     public void test() {
         log.info("test!!");
+    }
+
+    @Test
+    public void fluxSubscriber() {
+        Flux<String> stringFlux = Flux.just("Ridwan", "Bruno", "Sancho", "Ericsen", "Martial").log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Ridwan")
+                .expectNext("Bruno")
+                .expectNext("Sancho")
+                .expectNext("Ericsen")
+                .expectNext("Martial")
+                .expectComplete()
+                .verify();
+        StepVerifier.create(stringFlux)
+                .expectNext("Ridwan", "Bruno", "Sancho", "Ericsen", "Martial")
+                .verifyComplete();
+
+        // TODO: 8/8/22 could not test with bounded publisher. 
     }
 }
