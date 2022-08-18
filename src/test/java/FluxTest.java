e8edab9021ff62979d4691dca0b93570ad4165a9
ridwan <ridwan.ramanujon@gmail.com>
2022-08-19 04:33:09 +0600

    added new test

--- src/test/java/FluxTest.java
+++ src/test/java/FluxTest.java
@@ -1,7 +1,11 @@
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.List;
 
 @Slf4j
 public class FluxTest {
@@ -15,7 +19,9 @@ public class FluxTest {
     @Test
     public void fluxSubscriber() {
         Flux<String> stringFlux = Flux.just("Ridwan", "Bruno", "Sancho", "Ericsen", "Martial").log();
-
+        Flux<String> stringFlux1 = Flux.fromIterable(List.of("Ridwan", "Bruno", "Sancho", "Ericsen", "Martial"));
+        stringFlux.subscribe(a->log.info(a), Throwable::printStackTrace, () -> log.info("Done"),
+                subscription-> subscription.request(2)); //bounded subscription;
         StepVerifier.create(stringFlux)
                 .expectNext("Ridwan")
                 .expectNext("Bruno")
@@ -24,10 +30,35 @@ public class FluxTest {
                 .expectNext("Martial")
                 .expectComplete()
                 .verify();
-        StepVerifier.create(stringFlux)
+        StepVerifier.create(stringFlux1)
                 .expectNext("Ridwan", "Bruno", "Sancho", "Ericsen", "Martial")
                 .verifyComplete();
 
         // TODO: 8/8/22 could not test with bounded publisher. 
+    }
+
+    @Test
+    public void baseSubscriberTest() {
+                Flux<String> stringFlux = Flux.fromIterable(List.of("Ridwan", "Bruno", "Sancho", "Ericsen", "Martial")).log();
+
+                stringFlux.subscribe(new BaseSubscriber<String>() {
+                    final int request_count = 2;
+                    int count = 0;
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(request_count);
+                     }
+
+                    @Override
+                    protected void hookOnNext(String value) {
+                        count++;
+                        if(count>=request_count) {
+                            count = 0;
+                            request(request_count);
+                        }
+                    }
+                });
+
+
     }
 }
